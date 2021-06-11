70f5d03060f24740e75f92efc59678ac627ed31b
jfrance@opusonesolutions.com <jfrance@opusonesolutions.com>
2021-06-11 14:21:29 -0400

    when refactoring the commit log for tests it broke the actual app commits

--- idp_schedule_provider/forecaster/database.py
+++ idp_schedule_provider/forecaster/database.py
@@ -18,5 +18,9 @@ def get_db_session() -> Generator[Session, None, None]:
     db = SessionLocal()
     try:
         yield db
+        db.commit()
+    except Exception:
+        db.rollback()
+        raise
     finally:
         db.close()
